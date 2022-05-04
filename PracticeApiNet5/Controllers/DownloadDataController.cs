086987fda7da939c312ea2225cf504c3b2ada61f
John Szop <szopakleks@hotmail.com>
2022-05-04 18:57:25 +0200

    Geting data works.

---
+++ PracticeApiNet5/Controllers/DownloadDataController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticeApiNet5.Entities;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Collections.Generic;
+using System.Net;
+
+
+namespace PracticeApiNet5.Controllers
+{
+    [Route("api/[controller]")] 
+    [ApiController]
+    public class DownloadDataController : ControllerBase
+    {
+        
+
+        [SwaggerOperation(Summary = "Download data from jsonplaceholder")]
+        [HttpGet] // api/posts
+        public List<string> GetAllPosts()
+        {
+            var client = new WebClient();
+           List<string> posts = new List<string>();   
+
+            for (int i = 1; i < 5; i++)
+            {
+                var text = client.DownloadString($"http://jsonplaceholder.typicode.com/posts/{i}");
+                posts.Add(text);    
+            }
+
+            return posts;
+        }
+
+
+
+    }
+}
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace TestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            List<string> posts = new List<string>();
+
+            for (int i = 1; i < 5; i++)
+            {
+                var text = client.DownloadString($"http://jsonplaceholder.typicode.com/posts/{i}");
+                posts.Add(text);
+                
+            }
+
+            var cos = posts;
+        }
+    }
+}
